45d0576caf37a459577738ee34b3b27268aaa122
Disshook <ddulguun493@gmail.com>
2024-03-18 17:30:33 +0800

    hfg

--- src/hooks/axios.ts
+++ src/hooks/axios.ts
@@ -4,10 +4,8 @@ import { utga } from "./img";
 const axiosInstance = axios.create({
   baseURL:
     utga === true
-      ? "https://topgenius.tanuweb.cloud/api/v1"
-      : "http://localhost:9090/api/v1", // Set your API base URL here
-  // baseURL: "http://localhost:5000",
-  // timeout: 5000 // Set a timeout for requests (in milliseconds)
+      ? "https://topgeniuses.tanuweb.cloud/api/v1"
+      : "http://localhost:9090/api/v1",
 });
 
 // Add an interceptor to include the Bearer token in the request headers
