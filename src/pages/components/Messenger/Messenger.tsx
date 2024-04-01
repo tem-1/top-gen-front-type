4f45862e9d50860c79265b5b1c35eb0417215723
Disshook <ddulguun493@gmail.com>
2024-04-01 16:08:38 +0800

    fghfg

--- src/pages/components/Messenger/Messenger.tsx
+++ src/pages/components/Messenger/Messenger.tsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const Messenger = () => {
   return (
     <FacebookProvider appId="1085395405829630" chatSupport>
-      <CustomChat pageId="267523980049003" minimized={false} />
+      <CustomChat pageId="267523980049003" minimized={true} />
     </FacebookProvider>
   );
 };
